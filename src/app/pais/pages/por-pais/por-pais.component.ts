d1b6c1dad3f5ef30c325f77335cb34a655daf635
Felipe Buitrago <flipe282000@gmail.com>
2022-08-26 23:29:41 -0500

    with search autocomplete

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,7 +5,11 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
+  styles: [`
+    li{
+      cursor:pointer;
+    }
+  `
   ]
 })
 export class PorPaisComponent implements OnInit {
@@ -13,6 +17,9 @@ export class PorPaisComponent implements OnInit {
   termino  : string     = "";
   hayError : boolean    = false;
   paises   : Country[]  = [];
+
+  paisesSugeridos   : Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService:PaisService) { }
 
@@ -23,15 +30,27 @@ export class PorPaisComponent implements OnInit {
     console.log(this.termino);
     
     this.paisService.buscarPais(this.termino).subscribe(
-        (paises) => {this.paises = paises},
-          (error) => {this.hayError = true;this.paises = []}
+        (paises) => {this.paises = paises;this.mostrarSugerencias=false},
+          (error) => {this.hayError = true;this.mostrarSugerencias=false;this.paises = []}
     );
   }
 
-  sugerencias(){
+  sugerencias(termino : string){
 
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
     
+    this.paisService.buscarPais( termino )
+      .subscribe( 
+        paises => this.paisesSugeridos = paises.splice(0,5),
+        (err) => this.paisesSugeridos = []
+      );
+
+  }
+
+  buscarSugerido( termino: string ) {
+    this.buscar( termino );
   }
 
   ngOnInit(): void {
